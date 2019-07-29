d9943cef2f416c218276b943a7fc26ad42459d5d
WhizSid <whizsid@aol.com>
2019-07-30 01:57:55 +0530

    Prettier codes

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,21 +1,20 @@
-import React,{Component} from 'react';
-import {render} from 'react-dom';
-import Player from '../../src/Player';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Player from "../../src/Player";
 
 class App extends Component {
-    render(){
-        return (
-            <div>
-                uiiiiiii
-                <Player 
-                	apiKey="AIzaSyDwcGALDxWC1T-5fnGvlzxvIJIoghO0ZUc"
-                	language="en"
-                	zoom={14}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        uiiiiiii
+        <Player
+          apiKey="AIzaSyDwcGALDxWC1T-5fnGvlzxvIJIoghO0ZUc"
+          language="en"
+          zoom={14}
+        />
+      </div>
+    );
+  }
 }
 
-render(
-    <App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Player.js
+++ src/Player.js
@@ -1,87 +1,57 @@
-import React,{
-	Component
-} from 'react';
-import Paper from '@material-ui/core/Paper';
-import withStyles from '@material-ui/core/styles/withStyles';
-import {
-	Map,
-	GoogleApiWrapper
-} from 'google-maps-react';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 //import PropTypes from 'proptypes';
 //import PlayIcon from '@material-ui/icons/Play';
 //import PauseIcon from '@material-ui/icons/Pause';
 //import StopIcon from '@material-ui/icons/Stop';
 //import Typography from '@material-ui/core/Typography';
 
-const styles = theme=>({
-	paper:{
-		
-	}
-})
+const styles = theme => ({
+  paper: {}
+});
 
 class PlayerComponent extends Component {
-	
-	constructor(props){
-		super(props);
-		
-		this.handlePause = this.handlePause.bind(this);
-		this.handlePlay = this.handlePlay.bind(this);
-		this.handleStop = this.handleStop.bind(this);
-		this.handleSeek = this.handleSeek.bind(this);
-		this.handleForward = this.handleForward.bind(this);
-		this.handleBackward = this.handleBackward.bind(this);
-	}
-	
-	handlePause(){
-		
-	}
-	
-	handlePlay(){
-		
-	}
-	
-	handleStop(){
+  constructor(props) {
+    super(props);
 
-	}
-	
-	handleSeek(){
-		
-	}
-	
-	handleForward(){
-		
-	}
-	
-	handleBackward(){
-		
-	}
+    this.handlePause = this.handlePause.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleSeek = this.handleSeek.bind(this);
+    this.handleForward = this.handleForward.bind(this);
+    this.handleBackward = this.handleBackward.bind(this);
+  }
 
-	render(){
-		const {classes,google,zoom} = this.props;
-		
-		return (
-			<Paper>
-				<div className={classes.seekBar}>
-				  hhhhh
-				</div>
-				<Map
-					google={google}
-					zoom={zoom}
-				>
-				
-				</Map>
-			</Paper>
-		);
-	}
+  handlePause() {}
+
+  handlePlay() {}
+
+  handleStop() {}
+
+  handleSeek() {}
+
+  handleForward() {}
+
+  handleBackward() {}
+
+  render() {
+    const { classes, google, zoom } = this.props;
+
+    return (
+      <Paper>
+        <div className={classes.seekBar}>hhhhh</div>
+        <Map google={google} zoom={zoom} />
+      </Paper>
+    );
+  }
 }
 
-
-const Player =  GoogleApiWrapper(
-  (props) => ({
-    apiKey: props.apiKey,
-    language: props.language,
-  }
-))(withStyles(styles) (PlayerComponent));
+const Player = GoogleApiWrapper(props => ({
+  apiKey: props.apiKey,
+  language: props.language
+}))(withStyles(styles)(PlayerComponent));
 /*
 Player.propTypes = {
 	coordinates: 	PropTypes.arrayOf(PropTypes.shape({
@@ -106,4 +76,3 @@ Player.propTypes = {
 */
 
 export default Player;
-    
